b41c1f52a2fcd631b1c4923f69bf1584f1b53d54
Danny Crichton <danny.crichton@gmail.com>
2026-03-03 21:43:39 -0500

    Mostly fleshed out Scene 1 game state

--- gamestate.py
+++ gamestate.py
@@ -3,7 +3,7 @@ gamestate.py
 
 A dataclass for storing all data related to the game from scene to scene
 """
-
+import copy
 from dataclasses import dataclass, asdict, fields
 
 from character import Character
@@ -46,27 +46,97 @@ class SpendingDecision:
         return abs(self.mayor_likability) + abs(self.rep_likability) + abs(self.union_rate) + abs(self.substack_subs) + abs(self.admiral_likability) + abs(self.repairs)
 
 @dataclass
-class Scene1Decisions:
-    scene_decisions = {}
-    scene_decisions['Proposed Contracts'] = 0
-    scene_decisions['Blame'] = {
-        "Self Blame": 0,
-        "Navy": 0,
-        "Congress": 0,
-        "Local Government": 0,
-        "GBI": 0,
-        "Act of God": 0
-    }
+class SpendingDecisions:
+    mayor: SpendingDecision = SpendingDecision()
+    rep: SpendingDecision = SpendingDecision()
+    ceo: SpendingDecision = SpendingDecision()
+    substack: SpendingDecision = SpendingDecision()
+    union: SpendingDecision = SpendingDecision()
+    admiral: SpendingDecision = SpendingDecision()
+
+@dataclass
+class SceneDecisions:
+    scene_decision: dict = {}
 
     share_price: float = 0
     workforce: int = 10000
 
-    mayor_spending = SpendingDecision()
-    rep_spending = SpendingDecision()
-    ceo_spending = SpendingDecision()
-    substack_spending = SpendingDecision()
-    union_spending = SpendingDecision()
-    admiral_spending = SpendingDecision()
+    spending_decisions: SpendingDecisions = SpendingDecisions()
 
-def processScene1(state: GameState, decisions: Scene1Decisions):
-    pass
+def processScene1(state: GameState, decisions: SceneDecisions) -> GameState:
+    new_state = copy.deepcopy(state)
+
+    #Handle Scene Decisions
+    #1-1
+    new_state.contracts = decisions.scene_decision['Contracts']
+
+    #1-2
+    blame_sum = sum(decisions.scene_decision['Blame'].values()) #Players can choose 1 to all of the options and we scale accordingly
+    new_state.mayor_likability -= 20 * decisions.scene_decision['Blame']['Mayor Likability'] / blame_sum
+    new_state.rep_likability -= 20 * decisions.scene_decision['Blame']['Rep Likability'] / blame_sum
+    new_state.union_rate += 0.2* decisions.scene_decision['Blame']['GBI'] / blame_sum
+    new_state.admiral_likability = state.admiral_likability + 10 * decisions.scene_decision['Blame']['Self Blame'] / blame_sum - 20 * decisions.scene_decision['Blame']['Navy'] / blame_sum - 10 * decisions.scene_decision['Blame']['Act of God'] / blame_sum + 5 * (1-blame_sum)
+
+    return new_state
+
+
+def processSpending(state: GameState, spending_decisions: SpendingDecisions):
+    """
+    Processes in order all of the changes from the spending data from the 6 characters
+    """
+
+    # Mayor
+    state.repairs += spending_decisions.mayor.repairs
+    state.mayor_likability += spending_decisions.mayor.mayor_likability / 50000
+    state.rep_likability += spending_decisions.mayor.rep_likability / 25000
+    state.substack_subs *= 1.1 ^ (spending_decisions.mayor.substack_subs / 100000)
+    state.union_rate += 0.01 * spending_decisions.mayor.union_rate / 50000
+    state.admiral_likability += spending_decisions.mayor.admiral_likability / 250000
+    
+    state.mayor_funds -= spending_decisions.mayor.total()
+
+    # Representative
+    state.repairs += spending_decisions.rep.repairs
+    state.mayor_likability += spending_decisions.rep.mayor_likability / 100000
+    state.rep_likability += spending_decisions.rep.rep_likability / 200000
+    state.substack_subs *= 1.1 ^ (spending_decisions.rep.substack_subs / 250000)
+    state.union_rate += 0.01 * spending_decisions.rep.union_rate / 100000
+    state.admiral_likability += spending_decisions.rep.admiral_likability / 100000
+    
+    state.rep_funds -= spending_decisions.rep.total()
+
+    # CEO
+    state.repairs += spending_decisions.ceo.repairs
+    state.mayor_likability += spending_decisions.ceo.mayor_likability / 250000
+    state.rep_likability += spending_decisions.ceo.rep_likability / 250000
+    state.substack_subs *= 1.1 ^ (spending_decisions.ceo.substack_subs / 1000000)
+    state.union_rate += 0.01 * spending_decisions.ceo.union_rate / 250000
+    state.admiral_likability += spending_decisions.ceo.admiral_likability / 250000
+    
+    state.ceo_funds -= spending_decisions.ceo.total()
+
+    # Substacker
+    state.repairs += 250 * state.substack_subs * spending_decisions.substack.repairs
+    state.mayor_likability += spending_decisions.substack.mayor_likability * 5
+    state.rep_likability += spending_decisions.substack.rep_likability * 5
+    state.substack_subs *= 1.1 ^ (spending_decisions.substack.substack_subs)
+    state.union_rate += 0.05 * spending_decisions.substack.union_rate
+    state.admiral_likability += spending_decisions.substack.admiral_likability * 5
+
+    # Union President
+    state.repairs += spending_decisions.union.repairs
+    state.mayor_likability += spending_decisions.union.mayor_likability / 100000
+    state.rep_likability += spending_decisions.union.rep_likability / 100000
+    state.substack_subs *= 1.1 ^ (spending_decisions.union.substack_subs / 250000)
+    state.union_rate += 0.01 * spending_decisions.union.union_rate / 100000
+    state.admiral_likability += spending_decisions.union.admiral_likability / 100000
+    
+    state.union_funds -= spending_decisions.union.total()
+
+    # Admiral
+    state.repairs += 500000 * spending_decisions.admiral.repairs
+    state.mayor_likability += spending_decisions.admiral.mayor_likability * 5
+    state.rep_likability += spending_decisions.admiral.rep_likability * 5
+    state.substack_subs *= 1.1 ^ (spending_decisions.admiral.substack_subs)
+    state.union_rate += 0.05 * spending_decisions.admiral.union_rate
+    state.admiral_likability += spending_decisions.admiral.admiral_likability * 5
